8008315f40dd10d9b2322710ef7db017c9d85778
Brady <brady.johnston@me.com>
2021-02-08 14:42:01 +1100

    add @param

--- R/reading.R
+++ R/reading.R
@@ -1,20 +1,26 @@
 #' Read and Name COLVAR File
-#'  @export
+#' @param file File to read and name.
+#' @export
 readcolvar <- function(file) {
   df <- read.table(file)
   colnames(df) <- c("time", "path.sss", "path.zzz", "metad.rbias", "walls.bias", "rmsd0", "rmsd1")
+  df
 }
 
 #' Read and Name HILLS File
-#'  @export
+#' @param file File to read and name.
+#' @export
 readhills <- function(file) {
   df <- read.table(file)
   colnames(df) <- c("time", "path.sss", "path.zzz", "sigma_path.sss", "sigma_path.zzz", "height", "biasf")
+  df
 }
 
 #' Read and Name FES File
-#'  @export
+#' @param file File to read and name.
+#' @export
 readfes <- function(file) {
   df <- read.table(file)
   colanmes(df) <- c("sss.sss", "zzz.zzz", "ff1", "dff1_sss.sss", "dff1_zzz.zzz")
+  df
 }
