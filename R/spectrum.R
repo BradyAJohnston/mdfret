f4251b246daebc0634491f8d1733f5a0809e1515
Brady <brady.johnston@me.com>
2021-04-12 15:28:42 +0800

    remove wl_2_rb replace spectrum

---
+++ R/spectrum.R
@@ -0,0 +1,14 @@
+#' Data frame of visible light values.
+#'
+#'
+#' @docType csv
+#'
+#' @usage data(spectrum)
+#'
+#' @format Data frame with rgb data for visible spectrum.
+#
+#' @keywords datasets, visible, wavelength, nm
+#'
+#' @references Brady Johnston, unpublished work (as of Aug 2020)
+#'
+"spectrum"
